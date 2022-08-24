896d4c49b14ee0731251b50d1188b92d43a3ce28
hirokikumagai <hiro.corp1212@gmail.com>
2022-08-25 03:48:13 +0900

    試しにmainを書いてみた

---
+++ main.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hkumagai <hkumagai@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/13 09:17:59 by hkumagai          #+#    #+#             */
+/*   Updated: 2022/08/25 03:45:29 by hkumagai         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <signal.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <libc.h>
+
+int main(int argc, char const *argv[])
+{
+	pid_t pid;
+
+	pid = argc;
+
+	return 0;
+}
