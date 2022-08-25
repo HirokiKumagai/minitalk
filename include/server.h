569bf046f36c07922aa3d217e5fa0ae68b380b58
hirokikumagai <hiro.corp1212@gmail.com>
2022-08-25 12:28:01 +0900

    pid_tを使ってみる

---
+++ include/server.h
@@ -0,0 +1,12 @@
+#ifndef SERVER_H
+#define SERVER_H
+#include <signal.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <libc.h>
+
+#endif // !SERVER_H# define SERVER_H
--- server.c
+++ server.c
@@ -1,27 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   server.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hkumagai <hkumagai@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/13 09:17:59 by hkumagai          #+#    #+#             */
-/*   Updated: 2022/08/25 03:45:29 by hkumagai         ###   ########.fr       */
+/*   Updated: 2022/08/25 12:27:37 by hkumagai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <signal.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <errno.h>
-#include <libc.h>
-
+#include "./include/server.h"
 int main(int argc, char const *argv[])
 {
 	pid_t pid;
+	if (argc == 1)
+		return (0);
 
 	pid = argc;
 
