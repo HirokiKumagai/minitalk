547df90287c874718ab33e07578c3c86e82f932e
hirokikumagai <hiro.corp1212@gmail.com>
2022-08-28 17:11:32 +0000

    client側からヌル文字が来たら終了したとするため、isEndメンバーを追加。 関数の分割

--- include/server_bonus.h
+++ include/server_bonus.h
@@ -1,24 +1,26 @@
 #ifndef SERVER_BONUS_H
 # define SERVER_BONUS_H
-
 # define EXIT_ERROR 1
 # define BYTE_COUNT 8
 
 # include <signal.h>
 # include <unistd.h>
+# include <stdlib.h>
+# include <stdbool.h>
 # include <sys/types.h>
 # include <unistd.h>
-# include <stdlib.h>
 # include <stdio.h>
 # include <errno.h>
 # include "../lib/ft_printf/include/ft_printf.h"
+
 
 typedef struct s_sigchar
 {
 	int				count;
 	unsigned char	bit;
+	bool			isEnd;
 }				t_sigchar;
 
-t_sigchar	g_sigchar = {0, 0};
+t_sigchar	g_sigchar = {0, 0, true};
 
 #endif
--- src/server_bonus.c
+++ src/server_bonus.c
@@ -6,12 +6,11 @@
 /*   By: hkumagai <hkumagai@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/13 09:17:59 by hkumagai          #+#    #+#             */
-/*   Updated: 2022/08/26 17:27:02 by hkumagai         ###   ########.fr       */
+/*   Updated: 2022/08/28 17:08:54 by hkumagai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/server_bonus.h"
-
 static void	sigFunc(int sig, siginfo_t *info, void *ucontext)
 {
 	(void)info;
@@ -21,6 +20,7 @@ static void	sigFunc(int sig, siginfo_t *info, void *ucontext)
 		g_sigchar.bit <<= 1;
 	else if (sig == SIGUSR2 && g_sigchar.count < BYTE_COUNT)
 	{
+		g_sigchar.isEnd = false;
 		g_sigchar.bit |= 1;
 		g_sigchar.bit <<= 1;
 	}
@@ -29,18 +29,17 @@ static void	sigFunc(int sig, siginfo_t *info, void *ucontext)
 		if (sig == SIGUSR2)
 			g_sigchar.bit |= 1;
 		ft_putchar_fd(g_sigchar.bit, 1);
+		if (g_sigchar.bit == '\0')
+			ft_putstr_fd("end", 1);
 		g_sigchar.bit = 0;
 		g_sigchar.count = 0;
 	}
 }
 
-int	main(void)
+static void	receive_signal(void)
 {
-	pid_t				pid;
 	struct sigaction	act;
 
-	pid = getpid();
-	ft_printf("PID:	%d\n", pid);
 	ft_bzero(&act, sizeof(struct sigaction));
 	sigemptyset(&act.sa_mask);
 	act.sa_flags = SA_SIGINFO;
@@ -55,6 +54,15 @@ int	main(void)
 		ft_printf("Error: sigaction()\n");
 		exit(EXIT_ERROR);
 	}
+}
+
+int	main(void)
+{
+	pid_t		pid;
+
+	pid = getpid();
+	ft_printf("PID:	%d\n", pid);
+	receive_signal();
 	while (true)
 		pause();
 	return (0);
