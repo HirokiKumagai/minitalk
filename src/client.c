a73a06cca87b19c1fd2c87420426aeddeb06fad8
hirokikumagai <hiro.corp1212@gmail.com>
2022-08-30 04:57:40 +0000

    引数のチェックの修正 引数なしで呼び出すとセグフォになるのでチェック項目の見直し pidが存在しない時に終了させる

--- src/client.c
+++ src/client.c
@@ -6,7 +6,7 @@
 /*   By: hkumagai <hkumagai@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/13 09:17:59 by hkumagai          #+#    #+#             */
-/*   Updated: 2022/08/26 17:10:41 by hkumagai         ###   ########.fr       */
+/*   Updated: 2022/08/30 04:55:14 by hkumagai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,14 +32,16 @@ static int	ft_puterr(char *errmsg)
 	return (false);
 }
 
-static int	check_args(int argc, char const *argv[], pid_t pid)
+static int	check_args(int argc, char const *argv[])
 {
 	size_t	i;
 	size_t	len;
+	pid_t	pid;
 
 	if (argc != 3)
 		return (ft_puterr("invalid arg count"));
 	i = 0;
+	pid = ft_atoi(argv[1]);
 	len = ft_strlen(argv[1]);
 	while (i < len)
 	{
@@ -47,6 +49,8 @@ static int	check_args(int argc, char const *argv[], pid_t pid)
 			return (ft_puterr("invalid pid"));
 		i++;
 	}
+	if (kill(pid, 0) < 0)
+		return (ft_puterr("The pid does not exist."));
 	if (pid < DARWIN_PID_MIN || pid > DARWIN_PID_MAX)
 		return (ft_puterr("invalid pid of range"));
 	return (true);
@@ -77,9 +81,9 @@ int	main(int argc, char const *argv[])
 	pid_t	pid;
 	size_t	i;
 
-	pid = ft_atoi(argv[1]);
-	if (check_args(argc, argv, pid) == false)
+	if (check_args(argc, argv) == false)
 		exit(1);
+	pid = ft_atoi(argv[1]);
 	i = 0;
 	while (argv[2][i] != '\0')
 	{
--- src/client_bonus.c
+++ src/client_bonus.c
@@ -6,7 +6,7 @@
 /*   By: hkumagai <hkumagai@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/13 09:17:59 by hkumagai          #+#    #+#             */
-/*   Updated: 2022/08/29 22:37:08 by hkumagai         ###   ########.fr       */
+/*   Updated: 2022/08/30 04:55:28 by hkumagai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,16 @@ static int	ft_puterr(char *errmsg)
 	return (false);
 }
 
-static int	check_args(int argc, char const *argv[], pid_t pid)
+static int	check_args(int argc, char const *argv[])
 {
 	size_t	i;
 	size_t	len;
+	pid_t	pid;
 
 	if (argc != 3)
 		return (ft_puterr("invalid arg count"));
 	i = 0;
+	pid = ft_atoi(argv[1]);
 	len = ft_strlen(argv[1]);
 	while (i < len)
 	{
@@ -32,19 +34,22 @@ static int	check_args(int argc, char const *argv[], pid_t pid)
 			return (ft_puterr("invalid pid"));
 		i++;
 	}
+	if (kill(pid, 0) < 0)
+		return (ft_puterr("The pid does not exist."));
 	if (pid < DARWIN_PID_MIN || pid > DARWIN_PID_MAX)
 		return (ft_puterr("invalid pid of range"));
 	return (true);
 }
+
 
 int	main(int argc, char const *argv[])
 {
 	pid_t	server_pid;
 	char	*client_pid;
 
-	server_pid = ft_atoi(argv[1]);
-	if (check_args(argc, argv, server_pid) == false)
+	if (check_args(argc, argv) == false)
 		exit(EXIT_ERROR);
+	server_pid = ft_atoi(argv[1]);
 	receive_signal(output_signal);
 	send_message((char *)argv[2], server_pid);
 	client_pid = ft_itoa(getpid());
